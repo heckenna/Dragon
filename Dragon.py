dd0321000e0c42ab34f7aea2e362b8a6f95b684b
heckenna <heckenna@rose-hulman.edu>
2018-06-25 16:31:32 -0400

    darg

---
+++ Dragon.py
@@ -0,0 +1,33 @@
+###################################################
+#Noah Heckenlively
+#
+#
+#
+
+
+import time
+
+#Dragon by
+def dragon(): #with much of Nathaniel's help
+	#print("PREPARE TO MEET YOUR DOOM!!!")
+	#time.sleep(2.5)
+	print()
+	print("################")
+	print("      ##        ####")
+	print("     ####            ###        #")
+	print("        ####            #     ## ###            ##")
+	print("           ######        #   #      #####   ####")
+	print("                #####    #   #         #  #######")
+	print("                    #  #      #      ####   # ##")
+	print("              ####### ##     ##    ##           #")
+	print("          ####          #####     #")
+	print("        ##                       #")
+	print("       #                         #")
+	print("       #     #          #       #")
+	print("       #      # ######## #     #")
+	print("        #     #          #     #")
+	print("      #     #             #     #")
+	print("       #######             #######")
+	
+	
+dragon()
